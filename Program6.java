5596e8155651f7bf2c98f1a64bb53b765583d797
HridayNayak25 <hridaynayak8@gmail.com>
2024-08-21 22:00:43 -0700

    Update Program6.java
    Circle code 6 assignment

--- Program6.java
+++ Program6.java
@@ -1,9 +1,25 @@
-//import stuff here!
-
-//Your code here
-
-//Paste console output below:
-/*
-
-
-*/
+import java.util.Scanner;
+public class Circle_1{
+    public static void main(String[]args){
+        Scanner myScanner = new Scanner(System.in);
+        
+        final double pi = 3.14159;
+        double radius = 0.0;
+       
+        
+        System.out.println("radius: ");
+        radius = myScanner.nextDouble();
+        
+        double area = (pi*radius*radius);
+        double diameter = (2.0*radius);
+        double circumference = (2*pi*radius);
+        
+        System.out.println("Radius: " + radius);
+        System.out.println("Diameter: " + diameter);
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+    
+        
+        
+    }
+}
